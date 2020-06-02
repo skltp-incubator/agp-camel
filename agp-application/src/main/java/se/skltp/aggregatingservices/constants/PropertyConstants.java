47de821724175ce82e7935f98a69db5ae7fbd813
Hans Bragman <hans.bragman@soprasteria.com>
2020-06-02 16:40:17 +0200

    NMT-1722 HawtIO login

---
+++ agp-application/src/main/java/se/skltp/aggregatingservices/constants/PropertyConstants.java
@@ -0,0 +1,7 @@
+package se.skltp.aggregatingservices.constants;
+
+public class PropertyConstants {
+
+  public static final String HAWTIO_AUTHENTICATION_ENABLED = "hawtio.authentication.enabled";
+  public static final String HAWTIO_EXTERNAL_LOGINFILE = "hawtio.external.loginfile";
+}
